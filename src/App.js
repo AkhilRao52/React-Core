4fe51244bd1858e6dc21eeda154880cb791bbf4e
AkhilRao <raoakhil.akhil@gmail.com>
2019-07-14 18:25:44 -0400

    hooks - useState

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person'
 import './App.css';
 
-class App extends Component {
+const app = (props) => {
 
-  state = {
+  const [personState, setPersonState] = useState({
     persons: [
-      {
-        name: "Akhi",
-        age: 24
-      },
-      {
-        name: "Max",
-        age: 28
-      }
-  ]
-  }
+      {name: "Akhi",age: 24},
+      {name: "Max",age: 28}
+    ],
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const [otherState,setOtherState] = useState('some other value');
+
+  console.log(personState, otherState);
+
+  const switchNameHandler = () => {   
+    /* This doesn't get the other state property.
+    So we need to manually set it like mentioned above (state slices)*/ 
+    setPersonState({
       persons: [
-        {
-          name: "kingakhil",
-          age: 24
-        },
-        {
-          name: "Max",
-          age: 27
-        }
-    ]
+        {name: "kingakhil",age: 24},
+        {name: "Max",age: 27}
+      ],
     })
   }
 
-
-
-  render() {
     return (
       <div className="App">
        Names and Ages
-       <button onClick = {this.switchNameHandler}> Switch name </button>
-       <Person name= {this.state.persons[0].name} age= {this.state.persons[0].age}/>
-       <Person name= {this.state.persons[1].name} age= {this.state.persons[1].age}> He is a tutor </Person>
+       <button onClick = {switchNameHandler}> Switch name </button>
+       <Person name= {personState.persons[0].name} age= {personState.persons[0].age}/>
+       <Person name= {personState.persons[1].name} age= {personState.persons[1].age}> He is a tutor </Person>
        <Person/>
       </div>
     );
-  }
 }
 
-export default App;
+export default app;
